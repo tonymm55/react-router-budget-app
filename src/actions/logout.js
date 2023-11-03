498dd656a3c1bef03b0f5fd459493b5270a781bc
TonyMM55 <tonymmoran@gmail.com>
2023-11-03 19:11:48 +0000

    addition of forms and error handling

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,6 +1,9 @@
 // React Router Dom (RRD) imports
 import { redirect } from "react-router-dom";
 
+// library dependencies
+import { toast } from "react-toastify";
+
 // helper functions
 import { deleteItem } from "../helpers";
 
@@ -9,6 +12,7 @@ export async function logoutAction() {
   deleteItem({
     key: "userName"
   })
+  toast.success("You've deleted your account!")
   // return redirect
   return redirect("/")
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,12 @@
 // RRD imports 
 import { useLoaderData } from "react-router-dom";
 
+// library imports
+import { toast } from "react-toastify";
+
+// components
+import Intro from "../components/Intro";
+
 // helper functions
 import { fetchData } from "../helpers"
 
@@ -8,18 +14,31 @@ import { fetchData } from "../helpers"
 export function dashboardLoader() {
   const userName = fetchData("userName");
   return { userName }
+}
 
+// action
+export async function dashboardAction({request}) {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    throw new Error('Oh Ohhhh!')
+    // localStorage.setItem("userName", JSON.stringify(formData.userName));
+    // console.log("🚀 ~ dashboardAction ~ formData", formData);
+    // return toast.success(`Welcome, ${formData.userName}`);
+  } catch (e) {
+    throw new Error("There was a problem creating your account.");
+  }
 }
 
 const Dashboard = () => {
   const { userName } = useLoaderData()
 
   return (
-  <div>
-    <h2>{userName}</h2>
-    Dashboard
-  </div>
+  <>
+    {userName ? (<p>{userName}</p>) : 
+    <Intro />}
+  </>
   )
 }
 
-export default Dashboard
+export default Dashboard;
