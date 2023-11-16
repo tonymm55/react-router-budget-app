f6022b24e840ec91f12ff38f261651d60385316e
TonyMM55 <tonymmoran@gmail.com>
2023-11-16 13:27:17 +0000

    added AddBudgetForm component

---
+++ src/components/AddBudgetForm.jsx
@@ -0,0 +1,49 @@
+import { Form } from "react-router-dom"
+
+// library imports
+import { CurrencyDollarIcon } from "@heroicons/react/24/solid"
+
+const AddBudgetForm = () => {
+  return (
+    <div className="form-wrapper">
+      <h2 className="h3">
+        Create Budget
+      </h2>
+      <Form
+       method="post"
+       className="grid-sm"
+      >
+        <div className="grid-xs">
+          <label htmlFor="newBudget">Budget Name</
+          label>
+          <input 
+            type="text" 
+            name="newBudget" 
+            id="newBudget" 
+            placeholder="e.g. entertainment"
+            required
+          />
+        </div>
+        <div className="grid-xs">
+          <label htmlFor="newBudgetAmount">Amount</
+          label>
+          <input 
+            type="number" 
+            step="0.01"
+            name="newBudgetAmount" 
+            id="newBudgetAmount" 
+            placeholder="e.g. £350"
+            required
+            inputMode="decimal"
+          />
+        </div>
+        <button type="submit" className="btn btn--dark">
+          <span>Create Budget</span>
+          <CurrencyDollarIcon width={20} />
+        </button>
+      </Form>
+    </div>
+  )
+}
+
+export default AddBudgetForm
